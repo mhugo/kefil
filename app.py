0222518b4f236abaf0962afc494c7c0e8f12531b
Hugo Mercier <hugo@ternet.fr>
2025-07-30 21:43:36 +0200

    Revert "Try flaskwebgui"
    This reverts commit 63f32f1739d5dd2f0dbeebbd7e866a9dc7bd9812.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import datetime
 from flask import Flask, request, jsonify, render_template_string, send_from_directory
-from flaskwebgui import FlaskUI
 import sqlite3
 import json
 
@@ -282,5 +281,4 @@ def get_voucher():
 
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    FlaskUI(app=app, server="flask", fullscreen=True).run()
+    app.run(debug=True)
