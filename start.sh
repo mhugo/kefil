e9521e018622f9e3707da97dd4abe787e25fc421
Hugo Mercier <hugo@ternet.fr>
2025-07-30 23:07:08 +0200

    Fix start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 killall /home/diwan/kefil-main/venv/bin/python3
 cd ${SCRIPT_DIR}
-(sleep 3; firefox http://localhost:5000) &
+(sleep 3; /usr/bin/chromium --new-window --no-default-browser-check --allow-insecure-localhost --no-first-run --disable-sync --start-maximized --app=http://127.0.0.1:5000) &
 venv/bin/python3 app.py
