e31b672157e23e932a00382518834c008931a652
Hugo Mercier <hugo@ternet.fr>
2025-08-07 09:45:54 +0200

    Add logs

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 killall /home/diwan/kefil-main/venv/bin/python3
 cd ${SCRIPT_DIR}
 (sleep 3; /usr/bin/chromium --new-window --no-default-browser-check --allow-insecure-localhost --no-first-run --disable-sync --start-maximized --app=http://127.0.0.1:5000) &
-venv/bin/python3 app.py
+venv/bin/python3 app.py 2>&1 | tee -a logs.txt
+
