42a60ee6977ac8e87d1a6babcfad7f175264489b
Hugo Mercier <hugo@ternet.fr>
2025-11-08 17:16:36 +0100

    Add logs to a file

--- start.sh
+++ start.sh
@@ -4,4 +4,3 @@ killall /home/diwan/kefil-main/venv/bin/python3
 cd ${SCRIPT_DIR}
 (sleep 3; /usr/bin/chromium --new-window --no-default-browser-check --allow-insecure-localhost --no-first-run --disable-sync --start-maximized --app=http://127.0.0.1:5000) &
 venv/bin/python3 app.py 2>&1 | tee -a logs.txt
-
